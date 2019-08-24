004f9e28bab5a9bbdba12d910c3ad9fe1d8a58c4
Jason Bourne <m.bourne@wi.rr.com>
2019-08-24 07:48:17 -0500

    fixed navbar href warning

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,17 +5,17 @@ function Navbar (props) {
     return (
         <div className="container-fluid">
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark scoreBar centered">
-                <a className="navbar-brand" href="#">Flower Flitting</a>
+                <a className="navbar-brand" href="/">Flower Flitting</a>
                 {/* <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button> */}
                 <div id="navbarNav">
                     <ul className="navbar-nav">
                         <li className="nav-item"> 
-                            <a className="nav-link active" href="#">Guess: {(props.guessMessage ? props.guessMessage : "Click any flower to start")}</a>
+                            <a className="nav-link active" href="/">Guess: {(props.guessMessage ? props.guessMessage : "Click any flower to start")}</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link active" href="#">Score: {(props.score ? props.score : 0)} | Top Score: {(props.topScore ? props.topScore : 0)}</a>
+                            <a className="nav-link active" href="/">Score: {(props.score ? props.score : 0)} | Top Score: {(props.topScore ? props.topScore : 0)}</a>
                         </li>
                     </ul>
                 </div>
