4fc50d3b9adad615f0502ec8e913fccdb6655c95
Jason Bourne <m.bourne@wi.rr.com>
2019-08-24 07:39:18 -0500

    fixed navbar mobile styles

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,16 +6,16 @@ function Navbar (props) {
         <div className="container-fluid">
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark scoreBar centered">
                 <a className="navbar-brand" href="#">Flower Flitting</a>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                {/* <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarNav">
+                </button> */}
+                <div id="navbarNav">
                     <ul className="navbar-nav">
                         <li className="nav-item"> 
-                            <a className="nav-link" href="#">Guess: {(props.guessMessage ? props.guessMessage : "Click any flower to start")}</a>
+                            <a className="nav-link active" href="#">Guess: {(props.guessMessage ? props.guessMessage : "Click any flower to start")}</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">Score: {(props.score ? props.score : 0)} | Top Score: {(props.topScore ? props.topScore : 0)}</a>
+                            <a className="nav-link active" href="#">Score: {(props.score ? props.score : 0)} | Top Score: {(props.topScore ? props.topScore : 0)}</a>
                         </li>
                     </ul>
                 </div>
