3c7ee78c0a9c8eb815bc1d695667e24c5a6279e9
Jason Bourne <m.bourne@wi.rr.com>
2019-08-24 07:50:15 -0500

    Updates

---
+++ precache-manifest.2336f9f64141da0596c7350de8903135.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b71e4a506f700e091b1fdaa4a4ef52c",
+    "url": "/react-click-game/index.html"
+  },
+  {
+    "revision": "f2b0f44db8a54b515213",
+    "url": "/react-click-game/static/css/main.b51d14c1.chunk.css"
+  },
+  {
+    "revision": "d61801eb8c770b738079",
+    "url": "/react-click-game/static/js/2.75700cc7.chunk.js"
+  },
+  {
+    "revision": "f2b0f44db8a54b515213",
+    "url": "/react-click-game/static/js/main.a3b6dfdc.chunk.js"
+  },
+  {
+    "revision": "b123e1ebc39a2dc5b258",
+    "url": "/react-click-game/static/js/runtime~main.828b7891.js"
+  },
+  {
+    "revision": "53d2375e34af6d3731fb31bf1c7fa161",
+    "url": "/react-click-game/static/media/butterfly.53d2375e.jpg"
+  }
+]);
